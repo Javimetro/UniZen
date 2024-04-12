5b524d4ba2dfbf05a12519dec9c259969ed4ffea
javimetro <javierj@metropolia.fi>
2024-04-12 17:45:00 +0300

    insertMeasurements function in measurement-model.mjs adds only new measurements and rejects duplicated ones

--- server/src/models/measurement-model.mjs
+++ server/src/models/measurement-model.mjs
@@ -20,8 +20,27 @@ async function insertMeasurements(kubiosData, userId) {
         stress_index: item.result.stress_index
       };
 
+      // Define your SQL query for checking if a record already exists
+      const checkSql = `
+        SELECT * FROM measurements
+        WHERE user_id = ? AND timestamp = ?
+      `;
+
+      // Parameters for the check SQL query
+      const checkParams = [measurement.user_id, measurement.timestamp];
+
+      try {
+        // Check if a record already exists
+        const [existingRecords] = await promisePool.query(checkSql, checkParams);
+
+        // If a record already exists, skip this iteration
+        if (existingRecords.length > 0) {
+          console.log(`Measurement already exists for user ID: ${measurement.user_id} at timestamp: ${measurement.timestamp}`);
+          continue;
+        }
+
       // Define your SQL query for inserting data
-      const sql = `
+      const insertSql = `
         INSERT INTO measurements
           (user_id, timestamp, artefact, artefact_level, mean_hr_bpm, mean_rr_ms, pns_index,
            readiness, rmssd_ms, sd1_ms, sd2_ms, sdnn_ms, sns_index, stress_index)
@@ -30,7 +49,7 @@ async function insertMeasurements(kubiosData, userId) {
       `;
 
       // Parameters for the SQL query
-      const params = [
+      const insertParams = [
         measurement.user_id,
         measurement.timestamp,
         measurement.artefact,
@@ -47,16 +66,15 @@ async function insertMeasurements(kubiosData, userId) {
         measurement.stress_index
       ];
 
-      try {
-        // Assuming promisePool is your connection pool and query is a method to execute your SQL command
-        const [result] = await promisePool.query(sql, params);
-        console.log(`Inserted measurement with ID: ${result.insertId}`);
-      } catch (error) {
-        console.error('Error inserting measurement:', error);
-        // Handle the error appropriately in your application
-      }
+      // Insert the measurement
+      const [result] = await promisePool.query(insertSql, insertParams);
+      console.log(`Inserted measurement with ID: ${result.insertId}`);
+    } catch (error) {
+      console.error('Error inserting measurement:', error);
+      // Handle the error appropriately in your application
     }
   }
+}
 
 
 export { insertMeasurements };
